878e68b823636afbfb04bb8b4fc7b1c95d8b52d4
SSlhx <sofiane.salih.eca@gmail.com>
2025-05-07 15:01:18 +0200

    ajout formulaire login et register

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginForm from './components/LoginForm';
+import RegisterForm from './components/RegisterForm';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <Router>
+      <div className="App">
+        <h1>Connexion à mon app</h1>
+        <Routes>
+          {/* Route pour le formulaire de connexion */}
+          <Route path="/login" element={<LoginForm />} />
+          {/* Route pour le formulaire d'inscription */}
+          <Route path="/register" element={<RegisterForm />} />
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </Router>
+  );
 }
 
-export default App
-// import { useEffect, useState } from 'react';
-
-// function App() {
-//   const [posts, setPosts] = useState([]);
-
-//   useEffect(() => {
-//     fetch("http://localhost:8000/api/posts")
-//       .then((res) => res.json())
-//       .then((data) => setPosts(data["hydra:member"])); // pour API Platform
-//   }, []);
-
-//   return (
-//     <div>
-//       <h1>Articles</h1>
-//       <ul>
-//         {posts.map((post) => (
-//           <li key={post.id}>{post.title}</li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// }
-
-// export default App;
+export default App;
